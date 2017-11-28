702e541118e0606abd97dc5495636e1917ff560c
barnum <i@briangroat.com>
2017-11-28 14:31:22 -0500

    - Added basic data checking - Created functionality to change component state based on user input - Allowed user to update 2 different component state properties with simple data checking - Changing one aspect of component state does not affect the others

--- helloReact/public/app.jsx
+++ helloReact/public/app.jsx
@@ -7,14 +7,53 @@ var Greeter = React.createClass({
         };
     },
 
+    getInitialState: function () {
+        return {
+            name: this.props.name,
+            message: this.props.message
+        };
+    },
+
+    onButtonClick: function (e) {
+        e.preventDefault();
+
+        var nameRef = this.refs.name;
+        var name = nameRef.value;
+        nameRef.value = '';
+
+        var messageRef = this.refs.message
+        var message = messageRef.value;
+        messageRef.value = '';
+
+        if (typeof name === 'string' && name.length > 0) {
+            this.setState({
+                name: name
+            });
+        }
+
+        if (typeof message === 'string' && message.length > 0) {
+            this.setState({
+                message: message
+            });
+        }
+
+    },
+
     render: function() {
-        var name = this.props.name
-        var message = this.props.message
+        var name = this.state.name
+        var message = this.state.message
 
         return (
             <div>
-                <h1>{ name + '...'}</h1>
+                <h1>{ name }</h1>
                 <p>{ message }</p>
+
+                <form onSubmit={this.onButtonClick}>
+                    <input placeholder='name' type='text' ref='name' />
+                    <input placeholder='message' type='text' ref='message' />
+                    <br/>
+                    <button>Submit Form</button>
+                </form>
             </div>
         );
     }
